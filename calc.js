035621c25d0e066c48fc00207e2482a817b5baf9
breslavsky.anton@gmail.com <229835aA>
2023-04-05 19:59:57 +0100

    hate you

--- calc.js
+++ calc.js
@@ -1,13 +1,5 @@
-export function add(a, b) {
-    return a + b;
-}
-
 export function mult(a, b) {
     return a * b;
-}
-
-export function subs(a, b) {
-    return a - b;
 }
 
 export function div(a, b) {
